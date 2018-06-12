0b6f1dc48d6e8a70545e31b801d6bf580e9ddfe6
Ukidabek <ukidabek@gmail.com>
2018-06-12 19:57:50 +0200

    - Map generator update.

--- DungeonGenerator v2/MetaData/Editor/LayoutPropertyDrower.cs
+++ DungeonGenerator v2/MetaData/Editor/LayoutPropertyDrower.cs
@@ -14,20 +14,27 @@ namespace MapGenetaroion.DungeonGenerator.V2
         {
             layout = property.serializedObject.targetObject.GetType().GetField(property.propertyPath).GetValue(property.serializedObject.targetObject) as Layout;
 
-            return layout.RowsCount * EditorGUIUtility.singleLineHeight;
+            return (layout.RowsCount + 1) * EditorGUIUtility.singleLineHeight;
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
             {
-
+                Rect rect = position;
+                Rect labelRect = new Rect(position.position, new Vector2(position.width, EditorGUIUtility.singleLineHeight));
+                GUI.Label(labelRect, "Layout");
+                rect.y += EditorGUIUtility.singleLineHeight;
+                rect.size = new Vector2(EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight);
                 for (int i = layout.RowsCount - 1; i >= 0; i--)
                 {
                     for (int j = 0; j < layout.ColumnsCount; j++)
                     {
-
+                        layout[i, j] = GUI.Toggle(rect, layout[i, j], string.Empty);
+                        rect.x += rect.width;
                     }
+                    rect.y += EditorGUIUtility.singleLineHeight;
+                    rect.x = position.x; ;
                 }
             }
             EditorGUI.EndProperty();
